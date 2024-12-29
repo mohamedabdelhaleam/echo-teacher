4573ee045dfb5301be612440dae4d3def5aee74d
Mohamed Abdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-12-29 18:13:00 +0200

    Migration : Add Price to Group Table

--- database/migrations/2024_12_28_093202_create_groups_table.php
+++ database/migrations/2024_12_28_093202_create_groups_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->boolean('active')->default(true);
+            $table->integer('price');
             $table->foreignId('section_id')->constrained('sections')->onDelete('cascade');
             $table->timestamps();
         });
