2e36c60d5c20fba7d1ff2ca3ab3ed5e1f014cfd5
Mohamed Abdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-12-30 14:03:01 +0200

    Route : Add Group Routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Dashboard\DashboardController;
+use App\Http\Controllers\Dashboard\GroupController;
 use App\Http\Controllers\Dashboard\ProductController;
 use App\Http\Controllers\Dashboard\SectionController;
 use Illuminate\Support\Facades\Route;
@@ -29,6 +30,13 @@ Route::prefix('dashboard')->name('dashboard.')->middleware('auth')->group(functi
 
     Route::resource('products', ProductController::class);
     Route::resource('sections', SectionController::class);
+
+
+    Route::resource('groups', GroupController::class)->except(['index', 'show', 'create']);
+    Route::group(['prefix' => 'groups' , 'as' => 'groups.'], function () {
+        Route::get('{sectionId}', [GroupController::class, 'index'])->name('index');
+        Route::get('create/{sectionId}', [GroupController::class, 'create'])->name('create');
+    });
 
     ##################################### End Products Routes #####################################
 });
