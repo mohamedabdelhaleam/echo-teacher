9bdf21cd1de0f119ce2b262f07eddcbcc4adee3c
Mohamed Abdelhaleam <mohamedabdelhaleam13@gmail.com>
2025-01-20 18:28:44 +0200

    Add Relations to year model

--- app/Models/Year.php
+++ app/Models/Year.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Year extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function teacher()
+    {
+        return $this->belongsTo(User::class, 'teacher_id', 'id');
+    }
+
+    public function sections()
+    {
+        return $this->hasMany(section::class);
+    }
 }
