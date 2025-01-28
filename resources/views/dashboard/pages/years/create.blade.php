79ca9f4509de8973d66490a4482a955a977eb7e7
Mohamed Abdelhaleam <mohamedabdelhaleam13@gmail.com>
2025-01-28 19:54:05 +0200

    Create Add Year view

---
+++ resources/views/dashboard/pages/years/create.blade.php
@@ -0,0 +1,57 @@
+@extends('dashboard.layouts.app')
+@section('content')
+    <div class="contents">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breadcrumb-main">
+                        <div class="breadcrumb-action justify-content-center flex-wrap">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-12">
+                    <div class="card card-default card-md mb-4">
+                        <div class="card-header">
+                            <h6>أضافة صف دراسي</h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="">
+                                {{-- @include('message.message') --}}
+                                <form action="{{ route('dashboard.sections.store') }}" method="post">
+                                    @csrf
+                                    <div class="form-basic row">
+                                        <div class="col-md-6 mb-25">
+                                            <label>اسم الصف</label>
+                                            <input type="text" class="form-control form-control-lg" name="name"
+                                                required value="{{ old('name') }}">
+                                            <input type="hidden" class="form-control form-control-lg" name="year_id"
+                                                required value="{{ $yearId }}">
+                                            @if ($errors->has('name'))
+                                                <p class="text-danger">{{ $errors->first('name') }}</p>
+                                            @endif
+                                        </div>
+                                        <div class="col-md-6 mb-25">
+                                            <label>حالة الصف</label>
+                                            <select name="active" id="" class="form-control form-control-lg">
+                                                <option value="1">مفعل</option>
+                                                <option value="0">غير مفعل</option>
+                                            </select>
+                                            @if ($errors->has('active'))
+                                                <p class="text-danger">{{ $errors->first('active') }}</p>
+                                            @endif
+                                        </div>
+                                        <div class="form-group mb-0 col-12">
+                                            <button type="submit" class="btn btn-lg btn-primary btn-submit">أضافة
+                                                صف</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
